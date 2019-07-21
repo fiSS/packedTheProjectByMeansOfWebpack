339204e796c39a0bb5324269bd7a14b3e96794f2
Filipp <fis_@mail.ru>
2019-07-21 16:13:25 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+//в корень проекта
+window.addEventListener('DOMContentLoaded', function () {
+
+    "use strict";
+let calc = require('../js/parts/calc.js'),
+    form = require('../js/parts/form.js'),
+    slider = require('../js/parts/slider.js'),
+    tabs = require('../js/parts/tabs.js'),
+    timer = require('../js/parts/timer.js'),
+    modal = require('../js/parts/modal.js');
+
+    calc();
+    form();
+    slider();
+    tabs();
+    timer();
+    modal();
+
+});
